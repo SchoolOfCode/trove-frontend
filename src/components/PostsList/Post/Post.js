cedd9b3680f5d96a8ef0befb58697675b6694cb7
ryandonaghue <ryandonaghue@outlook.com>
2022-11-22 12:21:49 +0000

    Created Post component

---
+++ src/components/PostsList/Post/Post.js
@@ -0,0 +1,12 @@
+export default function Post() {
+    return (
+        <div>
+            <h2>Title</h2>
+            <p>Description</p>
+            <h3>Author</h3>
+            <h3>Date</h3>
+            <button>Link</button>
+        </div>
+      
+    )
+}; 
