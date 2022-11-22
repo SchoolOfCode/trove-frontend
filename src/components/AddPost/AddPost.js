3631a79788d33f07d877ec047bc4e86e4137e584
kianeblackman <kianeblackman@gmail.com>
2022-11-22 16:23:55 +0000

    AddPost css connected

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Form from './Form/Form';
+import './AddPost.css';
 
 export default function AddPost() {
   return (
-    <div>
+    <div className="add-post-container">
       <h2 className="form-title">Add Post</h2>
       <Form className="form" />
     </div>
