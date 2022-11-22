fcce3626f2048552a067d4e3bd5beaf62d60009f
Steve Beecheno <iamhacktinium@gmail.com>
2022-11-22 12:08:39 +0000

    Created top-level components & AddPost children

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import AddPost from './components/AddPost/AddPost';
+import Header from './components/Header/Header';
+import PostsList from './components/PostsList/PostsList';
 
 function App() {
   return (
     <div className="App">
+      <AddPost />
+      <Header />
+      <PostsList />
     </div>
   );
 }
---
+++ src/components/AddPost/AddPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from './Form/Form'
+
+export default function AddPost() {
+  return (
+    <div>
+      <h2>h2</h2>
+      <Form />
+    </div>
+  )
+}
