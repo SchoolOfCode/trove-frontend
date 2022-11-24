e8a75d9075ea705497e1f66b583bc6ed0ef3eb90
Steve Beecheno <iamhacktinium@gmail.com>
2022-11-24 23:11:19 +0000

    further styling

--- src/components/PostsList/Post/Post.js
+++ src/components/PostsList/Post/Post.js
@@ -5,18 +5,22 @@ export default function Post({ post }) {
 	return (
 		<div className="post">
 			<img className="post-img" src={post.thumbnail} alt="thumbnail"></img>
-			<h2 className="post-title">{post.title}</h2>
-			<p className="post-summary">{post.summary}</p>
-			<h3 className="post-author">{post.author}</h3>
-			<h3 className="post-date">{format(new Date(post.date_posted), "dd/MM/yyyy")}</h3>
-			<ul className="tags">
-				{post.tags.map((tag, index) => {
-					return <li key={index}>{tag}</li>;
-				})}
-			</ul>
-			<a className="post-url" href={post.url}>
-				Link
-			</a>
+			<div class="info">
+				<div class="inner-info">
+					<h2 className="post-title">{post.title}</h2>
+					<p className="post-summary">{post.summary}</p>
+					<h3 className="post-author">{post.author}</h3>
+					<h3 className="post-date">{format(new Date(post.date_posted), "dd/MM/yyyy")}</h3>
+				</div>
+				<ul className="tags">
+					{post.tags.map((tag, index) => {
+						return <li className="tag" key={index}>{tag}</li>;
+					})}
+				</ul>
+				<a className="post-url" href={post.url}>
+					Link
+				</a>
+			</div>
 		</div>
 	);
 }
