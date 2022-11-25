2596691aef55a296b6197dfdaa905d37ed3c2682
Steve Beecheno <iamhacktinium@gmail.com>
2022-11-25 08:59:09 +0000

    Card styling & visual bug fix

--- src/components/PostsList/Post/Post.js
+++ src/components/PostsList/Post/Post.js
@@ -6,15 +6,19 @@ export default function Post({ post }) {
 		<div className="post">
 			<img className="post-img" src={post.thumbnail} alt="thumbnail"></img>
 			<div class="info">
-				<div class="inner-info">
-					<h2 className="post-title">{post.title}</h2>
-					<p className="post-summary">{post.summary}</p>
-					<h3 className="post-author">{post.author}</h3>
-					<h3 className="post-date">{format(new Date(post.date_posted), "dd/MM/yyyy")}</h3>
-				</div>
+				<h2 className="post-title">{post.title}</h2>
+				<p className="post-summary">{post.summary}</p>
+				<h3 className="post-author">{post.author}</h3>
+				<h3 className="post-date">{format(new Date(post.date_posted), "dd/MM/yyyy")}</h3>
+			</div>
+			<div class="tagsnlink">
 				<ul className="tags">
 					{post.tags.map((tag, index) => {
-						return <li className="tag" key={index}>{tag}</li>;
+						return (
+							<li className="tag" key={index}>
+								{tag}
+							</li>
+						);
 					})}
 				</ul>
 				<a className="post-url" href={post.url}>
