b506ba9cd1be58d765fba4c5add966768c2c1f14
Steve Beecheno <iamhacktinium@gmail.com>
2022-11-25 09:10:13 +0000

    Fixed error msg

--- src/components/PostsList/Post/Post.js
+++ src/components/PostsList/Post/Post.js
@@ -5,13 +5,13 @@ export default function Post({ post }) {
 	return (
 		<div className="post">
 			<img className="post-img" src={post.thumbnail} alt="thumbnail"></img>
-			<div class="info">
+			<div className="info">
 				<h2 className="post-title">{post.title}</h2>
 				<p className="post-summary">{post.summary}</p>
 				<h3 className="post-author">{post.author}</h3>
 				<h3 className="post-date">{format(new Date(post.date_posted), "dd/MM/yyyy")}</h3>
 			</div>
-			<div class="tagsnlink">
+			<div className="tagsnlink">
 				<ul className="tags">
 					{post.tags.map((tag, index) => {
 						return (
