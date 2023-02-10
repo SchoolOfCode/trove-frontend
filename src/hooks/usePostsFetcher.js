ffc5253dc07feccb510edcb66757fdad16bda462
OttCoder <101673336+Ottcoder@users.noreply.github.com>
2023-02-10 12:25:42 +0000

    Changed fetch and post requests to link to deployed backend via env variables

--- src/hooks/usePostsFetcher.js
+++ src/hooks/usePostsFetcher.js
@@ -2,20 +2,23 @@ import { useEffect, useState } from "react";
 
 export default function usePostsFetcher() {
   const [posts, setPosts] = useState([]);
-  
-  useEffect(() => {
-  const getPosts = async () => {
-    const response = await fetch("http://localhost:3005/api/posts", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    setPosts(data.payload);
-  };
 
-  getPosts();
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ENDPOINT}/api/posts`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      setPosts(data.payload);
+    };
+
+    getPosts();
   }, []);
 
   return posts;
